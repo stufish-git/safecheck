ffaabc074943bcffaf7dbab4f0a8558b3dd9959f
stufish-git <62847474+stufish-git@users.noreply.github.com>
2026-02-28 01:21:21 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 //  Caches all app assets for full offline support
 // ═══════════════════════════════════════════════════════
 
-const CACHE_NAME = 'safechecks-v52';
+const CACHE_NAME = 'safechecks-v54';
 const ASSETS = [
   './',
   './index.html',
